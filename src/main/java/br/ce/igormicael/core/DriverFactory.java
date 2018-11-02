88bc95e0c5312646585f6521609537cdfd106f00
igor.m.peixoto <igor.m.peixoto@gmail.com>
2018-11-01 22:19:35 -0300

    Alteração no codigo para a utilização do selenium grid

--- src/main/java/br/ce/igormicael/core/DriverFactory.java
+++ src/main/java/br/ce/igormicael/core/DriverFactory.java
@@ -1,9 +1,17 @@
 package br.ce.igormicael.core;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxProfile;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import br.ce.igormicael.core.Propriedades.TipoExecucao;
 
 public class DriverFactory {
 
@@ -24,13 +32,35 @@ public class DriverFactory {
 	
 	public static WebDriver initDriver() {
 		WebDriver driver = null;
-		
-		System.setProperty("webdriver.gecko.driver", 
-				"/media/igor/Grande Igor/Desenvolvimento/webdrivers/geckodriver");
+		if(Propriedades.TIPO_EXECUCAO == TipoExecucao.LOCAL) {
+			System.setProperty("webdriver.gecko.driver", 
+					"/media/igor/Grande Igor/Desenvolvimento/webdrivers/geckodriver");
+				
+			switch (Propriedades.BROWSER) {
+				case FIREFOX: driver = new FirefoxDriver(); break;
+				case CHROME: driver = new ChromeDriver(); break;
+			}
+		}else {
+			DesiredCapabilities cap = null;
+			switch (Propriedades.BROWSER) {
+				case FIREFOX: 
+
+					cap = new DesiredCapabilities();
+					
+					FirefoxProfile fp = new FirefoxProfile();
+					fp.setPreference("network.proxy.type", 0);
+					cap.setCapability(FirefoxDriver.PROFILE, fp);
+					
+					
+					break;
+				case CHROME: cap=DesiredCapabilities.chrome(); break;
+			}
 			
-		switch (Propriedades.browser) {
-			case FIREFOX: driver = new FirefoxDriver(); break;
-			case CHROME: driver = new ChromeDriver(); break;
+			try {
+				driver = new RemoteWebDriver(new URL("http://192.168.25.72:4444/wd/hub"), cap);
+			} catch (MalformedURLException e) {
+				System.out.println("Falha na conexão com o grid");
+			}
 		}
 		
 		driver.manage().window().setSize(new Dimension(600, 800));
--- src/main/java/br/ce/igormicael/core/Propriedades.java
+++ src/main/java/br/ce/igormicael/core/Propriedades.java
@@ -4,13 +4,20 @@ public class Propriedades {
 
 	public static boolean FECHAR_BROWSER = true;
 	
-	public static Browsers browser = Browsers.FIREFOX;
+	public static Browsers BROWSER = Browsers.FIREFOX;
 	
 	public static String NOME_CONTA = "Conta " + System.nanoTime();
+	
+	public static TipoExecucao TIPO_EXECUCAO = TipoExecucao.GRID;
 	
 	public enum Browsers{
 		CHROME,
 		FIREFOX
+	}
+	
+	public enum TipoExecucao{
+		LOCAL,
+		GRID
 	}
 	
 }
